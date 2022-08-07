23e9b833f46ad3c2cb7cb5134ec8e95279841aa6
asbubam <asbubam@gmail.com>
2022-08-07 13:01:19 +0900

    Add README.md file

---
+++ README.md
@@ -0,0 +1,10 @@
+# TrustMe
+a simple CLI to manage Trust Relationship of AWS IAM Role  
+Trust me. You can add and remove your identity to `Trust Relastionship` easily.
+
+If you want to know about Trust Relationship, then recommend read this article: https://aws.amazon.com/blogs/security/how-to-use-trust-policies-with-iam-roles/
+
+## Install
+
+## Run TrustMe.
+
--- cmd/trustme/root.go
+++ cmd/trustme/root.go
@@ -8,10 +8,13 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "trustme",
-	Short: "trustme - a simple CLI to manage Trust Relationship of AWS IAM Role",
-	Long:  `trustme - a simple CLI to manage Trust Relationship of AWS IAM Role.
-If you want to know about Trust Relationship, then recommend read this article: https://aws.amazon.com/blogs/security/how-to-use-trust-policies-with-iam-roles/`,
-	Run:   func(cmd *cobra.Command, args []string) {
+	Short: "TrustMe - a simple CLI to manage Trust Relationship of AWS IAM Role",
+	Long: `TrustMe - a simple CLI to manage Trust Relationship of AWS IAM Role.
+
+If you want to know about Trust Relationship, then recommend read this article: 
+  https://aws.amazon.com/blogs/security/how-to-use-trust-policies-with-iam-roles/`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
 	},
 }
 
